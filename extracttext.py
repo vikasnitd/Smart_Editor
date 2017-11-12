44b411994ea4efba8024eecb54f6f06e3efa38c1
Hasan Alirajpurwala <hasan.150295@gmail.com>
2017-11-12 14:28:48 +0530

    Update extracttext.py
    Commented the file

--- extracttext.py
+++ extracttext.py
@@ -1,10 +1,10 @@
 from PIL import Image
 import pytesseract
 
-im = Image.open("img.jpg")
+im = Image.open("img.jpg")  # creating an image object
 
-text = pytesseract.image_to_string(im, lang="eng" )
+text = pytesseract.image_to_string(im, lang="eng" ) #recognize text from the image 
 
-print(text)
+print(text) #print the text
 
 
